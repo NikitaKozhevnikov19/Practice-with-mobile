b73d860c9d64d8a9ce7eff0539276cb9225bbd75
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-12 22:13:08 +0500

    renamed to IosTests.java

---
+++ src/test/java/tests/IosTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+@Tag("ios")
+public class IosTests extends TestBase {
+    pages.ios.SearchScreen screen = new pages.ios.SearchScreen();
+
+    @Test
+    @Owner("Kozherka")
+    @DisplayName("iOS: Проверка перехода в UI Elements")
+    void checkUiElementsTest() {
+        open(); // Принудительно открываем сессию для iOS
+        screen.clickUIElements();
+        screen.checkTextButtonVisible();
+    }
+
+    @Test
+    @Owner("Kozherka")
+    @DisplayName("iOS: Проверка возврата на главный экран")
+    void testBackNavigation() {
+        open();
+        screen.clickUIElements();
+        screen.goBack();
+        screen.clickUIElements();
+    }
+}
