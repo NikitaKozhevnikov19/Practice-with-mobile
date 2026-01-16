357512f4766f4cb0ac368029019ed193e636ecc6
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-16 17:43:24 +0500

    added MobileConfig.java

---
+++ src/test/java/config/MobileConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "classpath:${deviceHost}.properties"
+})
+public interface MobileConfig extends Config {
+
+    @Key("device")
+    String deviceName();
+
+    @Key("version")
+    String platformVersion();
+
+    @Key("app")
+    String appUrl();
+
+    @Key("bs.user")
+    String bsUser();
+
+    @Key("bs.key")
+    String bsKey();
+
+    @Key("remote.url")
+    String remoteUrl();
+
+    @Key("appium.automationName")
+    @DefaultValue("UiAutomator2")
+    String automationName();
+}
