ace488207b2821e2d87f02294bd17fe10a598289
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-16 17:42:16 +0500

    update AndroidDriverProvider.java

--- src/test/java/drivers/AndroidDriverProvider.java
+++ src/test/java/drivers/AndroidDriverProvider.java
@@ -1,12 +1,11 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
-import config.AndroidConfig;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import tests.TestBase;
 
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
@@ -14,25 +13,25 @@ import java.net.URL;
 import java.util.Map;
 
 public class AndroidDriverProvider implements WebDriverProvider {
-    private final AndroidConfig config = ConfigFactory.create(AndroidConfig.class, System.getProperties());
 
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName(config.androidDevice())
-                .setPlatformVersion(config.androidVersion())
-                .setApp(config.androidApp());
+
+        options.setDeviceName(TestBase.config.deviceName())
+                .setPlatformVersion(TestBase.config.platformVersion())
+                .setApp(TestBase.config.appUrl());
 
         options.setCapability("bstack:options", Map.of(
-                "userName", config.bsUser(),
-                "accessKey", config.bsKey(),
+                "userName", TestBase.config.bsUser(),
+                "accessKey", TestBase.config.bsKey(),
                 "projectName", "Wikipedia Android Project",
                 "buildName", "android-build-2026"
         ));
 
         try {
-            return new AndroidDriver(new URL(config.remoteUrl()), options);
+            return new AndroidDriver(new URL(TestBase.config.remoteUrl()), options);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
