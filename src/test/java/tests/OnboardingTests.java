e3294cd492efe6e2feeff058736e798f930c4dd9
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-18 22:36:11 +0500

    update OnboardingTests.java

--- src/test/java/tests/OnboardingTests.java
+++ src/test/java/tests/OnboardingTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.android.OnboardingScreen;
+import static com.codeborne.selenide.Selenide.open;
 
 @Tag("android")
 public class OnboardingTests extends TestBase {
@@ -15,7 +16,7 @@ public class OnboardingTests extends TestBase {
     @Owner("Kozherka")
     @DisplayName("Android: Проход onboarding Wikipedia")
     void wikipediaOnboardingTest() {
-
+        open();
         onboarding.swipeForward();
         onboarding.swipeForward();
         onboarding.swipeForward();
