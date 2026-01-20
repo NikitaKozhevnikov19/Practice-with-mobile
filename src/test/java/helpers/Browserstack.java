a7177e27d4ee11576a36e75de732d210cce6d544
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-01-20 23:09:11 +0500

    update Browserstack.java

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -6,7 +6,7 @@ import static io.restassured.RestAssured.given;
 
 public class Browserstack {
     public static String videoUrl(String sessionId) {
-        String url = String.format("api.browserstack.com", sessionId);
+        String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
                 .auth().basic(TestBase.config.bsUser(), TestBase.config.bsKey())
